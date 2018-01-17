cb4f89cbe1b5f1395dc00c1fe8a13e955b3aaf13
SFG147 <31681175+SFG147@users.noreply.github.com>
2018-01-17 10:15:59 -0500

    seat01
    Seating the green light.

---
+++ js/vidPlayer.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+/*
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
+var mediaPlayer;
+var btns = [btn1, btn2, btn3, btn4, btn5, bnt6, btn7, btn8, btn9, btn10];
+function initialiseMediaPlayer() {
+   mediaPlayer = document.getElementById('media-video');
+   mediaPlayer.controls = true;
+}
+video.src = 'new/url/video.ogv';
+video.load();  //Load the new video
+video.play();  //Play it
+*/
